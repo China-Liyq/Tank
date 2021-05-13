aa3e0a0084faea654ea179aab4902de2b2aa12b4
13826122459 <908337205@qq.com>
2021-05-13 21:31:43 +0800

    提交
    1

--- src/main/java/com/chinaliyq/App.java
+++ src/main/java/com/chinaliyq/App.java
@@ -15,8 +15,8 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-        new MainController().start();
 //        baseVersion();
+        new MainController().start();
     }
 
     private static void baseVersion() {
